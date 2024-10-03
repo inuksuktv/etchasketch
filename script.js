ce77a0c09c39f0e3ca86bf8d472ac13dccda423d
Bradley Morin <bradleymorin@gmail.com>
2024-10-03 12:41:04 -0600

    Create grid squares with javascript.

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+}
